1ce87b58e7461acd33f3cd2627b3eff8bc794fa7
MrRacoonGH <hadrien181107@gmail.com>
2025-02-11 17:24:40 +0100

    update 0.1

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 
+const $gameScreenGridCells = document.querySelectorAll(".game-cell")
+
+
+let currentPlayer = "y"
 let gameBoard = [
     ["", "", "", "", "", "", ""],
     ["", "", "", "", "", "", ""],
@@ -10,3 +14,28 @@ let gameBoard = [
 
 
 console.log(gameBoard)
+
+$gameScreenGridCells.forEach(function($gameScreenGridCell) {
+    $gameScreenGridCell.innerHTML = ""
+})
+
+$gameScreenGridCells.forEach(function($gameScreenGridCell) {
+    $gameScreenGridCell.addEventListener("click", function() {
+        const dataX = $gameScreenGridCell.getAttribute("data-x")
+
+        for (let i = 5; i >= 0; i--) {
+            if (gameBoard[i][dataX] === "") {
+              gameBoard[i][dataX] = currentPlayer;
+              break;
+            }
+          }
+
+          console.log(gameBoard)
+    })
+})
+
+
+//solution
+// gameBoard = currentPlayer;
+// break;
+

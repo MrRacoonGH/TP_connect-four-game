957bbe89624de34202f5c3fd7603e52fccc65fe2
MrRacoonGH <hadrien181107@gmail.com>
2025-02-17 09:23:57 +0100

    checkwin-v1

--- js/app.js
+++ js/app.js
@@ -42,7 +42,62 @@ $newGameScreenNewGameCpuPlayer.addEventListener("click", function() {
 })
 
 
-//solution
-// gameBoard = currentPlayer;
-// break;
+// checkwin
+
+function checkWin(board, player) {
+
+    for (let row = 0; row < 6; row++) {
+        for (let col = 0; col < 4; col++) {
+            if (
+                board[row][col] === player &&
+                board[row][col + 1] === player &&
+                board[row][col + 2] === player &&
+                board[row][col + 3] === player
+            ) {
+                return true;
+            }
+        }
+    }
+
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 7; col++) {
+            if (
+                board[row][col] === player &&
+                board[row + 1][col] === player &&
+                board[row + 2][col] === player &&
+                board[row + 3][col] === player
+            ) {
+                return true;
+            }
+        }
+    }
+
+    for (let row = 3; row < 6; row++) {
+        for (let col = 0; col < 4; col++) {
+            if (
+                board[row][col] === player &&
+                board[row - 1][col + 1] === player &&
+                board[row - 2][col + 2] === player &&
+                board[row - 3][col + 3] === player
+            ) {
+                return true;
+            }
+        }
+    }
+
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 4; col++) {
+            if (
+                board[row][col] === player &&
+                board[row + 1][col + 1] === player &&
+                board[row + 2][col + 2] === player &&
+                board[row + 3][col + 3] === player
+            ) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
 
